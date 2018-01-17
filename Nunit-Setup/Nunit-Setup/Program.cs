e6c01bef7b929a0a53fcedc17e3aefbff785984f
jnprogrammer <joshuanparker@gmail.com>
2018-01-17 08:00:03 -0500

    Set up Nunit testing

---
+++ Nunit-Setup/Nunit-Setup/Program.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+//using NUnit.Framework;
+//using OpenQA.Selenium.Firefox;
+
+//namespace Nunit_Setup
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            Console.WriteLine("Let's get that Data!");
+//            var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+//            foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
+//        }
+//    }
+//}
+
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace Nunit_Setup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        FirefoxDriver foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+        [SetUp]
+        public void Initialize()
+        {
+
+            var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            foxDriver.Close();
+        }
+
+
+    }
+}
+
---
+++ Nunit-Setup/Nunit-Setup/test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace Nunit_Setup
+{
+    class test
+    {
+        FirefoxDriver foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+        [SetUp]
+        public void Initialize()
+        {
+
+          var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+        }
+            
+        [Test]
+        public void OpenAppTest()
+        {
+           foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            foxDriver.Close();
+        }
+          
+       
+    }
+}
