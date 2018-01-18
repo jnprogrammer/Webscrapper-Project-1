a8019a8d5b30ffa2ab47a7368f2ba4dfbc4a60cd
jnprogrammer <joshuanparker@gmail.com>
2018-01-17 21:57:21 -0500

    Fixed issue with opening drivers in project

--- Nunit-Setup/Nunit-Setup/Program.cs
+++ Nunit-Setup/Nunit-Setup/Program.cs
@@ -1,24 +1,9 @@
-﻿//using System;
-//using NUnit.Framework;
-//using OpenQA.Selenium.Firefox;
-
-//namespace Nunit_Setup
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-
-//            Console.WriteLine("Let's get that Data!");
-//            var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
-//            foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
-//        }
-//    }
-//}
-
-using System;
+﻿using System;
+using System.IO;
 using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 
 namespace Nunit_Setup
 {
@@ -26,28 +11,31 @@ namespace Nunit_Setup
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Let's get that Data!");
+            
+            IWebDriver chromeDriver = new ChromeDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+            chromeDriver.Navigate().GoToUrl("https://coinmarketcap.com/all/views/all/");
+
+            var body = chromeDriver.FindElement(By.TagName("body")).Text;
+
+            Console.WriteLine("Got that Data!");
+
+
         }
-        FirefoxDriver foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
-        [SetUp]
-        public void Initialize()
-        {
-
-            var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
-        }
-
-        [Test]
-        public void OpenAppTest()
-        {
-            foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
-        }
-
-        [TearDown]
-        public void EndTest()
-        {
-            foxDriver.Close();
-        }
-
-
     }
 }
 
+
+
+/*
+ *
+ *IWebDriver foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+            //foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
+
+            foxDriver.Navigate().GoToUrl("https://coinmarketcap.com/all/views/all/");
+            foxDriver.Navigate().Refresh();
+            var textID = foxDriver.FindElement(By.Id("id-bitcoin"));
+            Console.WriteLine(textID);
+ * 
+ */
--- Nunit-Setup/Nunit-Setup/test.cs
+++ Nunit-Setup/Nunit-Setup/test.cs
@@ -6,18 +6,19 @@ namespace Nunit_Setup
 {
     class test
     {
+        
         FirefoxDriver foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
         [SetUp]
         public void Initialize()
         {
 
-          var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
+            var foxDriver = new FirefoxDriver(@"C:\Users\jnprogrammer9\Documents\Projects\c#\webScrapper-Selenium\Nunit-Setup\Nunit-Setup");
         }
-            
+
         [Test]
         public void OpenAppTest()
         {
-           foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
+            foxDriver.Url = "https://coinmarketcap.com/all/views/all/";
         }
 
         [TearDown]
@@ -25,7 +26,8 @@ namespace Nunit_Setup
         {
             foxDriver.Close();
         }
-          
-       
+
+
     }
 }
+
