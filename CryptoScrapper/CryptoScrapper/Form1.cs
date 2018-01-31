e740edd01cf87505d5d6d0568cf4c1e1bec86cb2
jnprogrammer <joshuanparker@gmail.com>
2018-01-30 20:10:32 -0500

    adding Enity framework

---
+++ CryptoScrapper/CryptoScrapper/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+
+namespace CryptoScrapper
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            IWebDriver chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl("https://coinmarketcap.com/all/views/all/");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ CryptoScrapper/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].CryptoCurrencyTable
+
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Symbol] NCHAR(10) NOT NULL, 
+    [Market Cap] NCHAR(10) NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    [Circuleating Supply] INT NULL, 
+    [Volume (24hr)] MONEY NULL, 
+    [1 Hour change] FLOAT NULL, 
+    [24 Hour change] FLOAT NULL, 
+    [7 Day change] FLOAT NULL
+)
