c3bf0067413dee8b9d796bea76d352013cfda9ec
jnprogrammer <joshuanparker@gmail.com>
2018-02-19 20:20:08 -0500

    imporved tests

---
+++ UnitTesting-Nunit/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Fundamentals.Math _math;
+        //Setup
+
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
+        //TearDown
+
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+    
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]   
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
