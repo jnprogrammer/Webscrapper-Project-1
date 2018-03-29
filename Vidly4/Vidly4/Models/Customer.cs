d770121fa8aa21f81ef79de06e2cfa1e474d881b
jnprogrammer <joshuanparker@gmail.com>
2018-03-29 18:03:35 -0400

    updated database to have data annotations

--- Vidly4/Vidly4/Models/Customer.cs
+++ Vidly4/Vidly4/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using Vidly4.Models;
 
 namespace Vidly4.Models
@@ -9,6 +10,8 @@ namespace Vidly4.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
