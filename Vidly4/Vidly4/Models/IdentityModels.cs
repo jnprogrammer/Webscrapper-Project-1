5c2ed4bd48462076ccce2615f64a61a80165226a
jnprogrammer <joshuanparker@gmail.com>
2018-03-29 14:00:20 -0400

    added Database Seed and updated appropatly

--- Vidly4/Vidly4/Models/IdentityModels.cs
+++ Vidly4/Vidly4/Models/IdentityModels.cs
@@ -14,7 +14,7 @@ namespace Vidly4.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
-            return userIdentity;
+            return userIdentity; 
         }
     }
 
