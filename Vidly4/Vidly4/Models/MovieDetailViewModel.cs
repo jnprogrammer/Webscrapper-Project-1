c73162c30f6f841f1b50d00dcdcbca8c105e8598
jnprogrammer <joshuanparker@gmail.com>
2018-04-10 20:18:35 -0400

    Publishing current project state before making a possible application breaking change

---
+++ Vidly4/Vidly4/Models/MovieDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly4.Models;
+
+namespace Vidly4.Views.ViewModels
+{
+    public class MovieDetailViewModel
+    {
+       public String Genre { get; set; }
+       public DateTime ReleaseDate { get; set; }
+       public DateTime AddDate { get; set; }
+       public int UnitsInStock { get; set; }
+    }
+}
