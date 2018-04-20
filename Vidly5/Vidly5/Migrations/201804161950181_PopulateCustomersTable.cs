3a2ac2e1ab5c7e27a90ed23a132428ef173c218c
jnprogrammer <joshuanparker@gmail.com>
2018-04-20 14:54:59 -0400

    Added details page to movies

--- Vidly5/Vidly5/Migrations/201804161950181_PopulateCustomersTable.cs
+++ Vidly5/Vidly5/Migrations/201804161950181_PopulateCustomersTable.cs
@@ -11,11 +11,10 @@ namespace Vidly5.Migrations
             Sql("INSERT INTO Customers (Name, IsSubscribed, BirthDay, MembershipTypeId) VALUES ('Josh Parker',1,'19891125', 3)");
             Sql("INSERT INTO Customers (Name, IsSubscribed, BirthDay, MembershipTypeId) VALUES ('A Pineapple in a human suit',0,'20520611', 1)");
             Sql("INSERT INTO Customers (Name, IsSubscribed, BirthDay, MembershipTypeId) VALUES ('Fizz Buzz', 1,'19721015', 2)");
-
         }
-
         public override void Down()
         {
+
         }
     }
 }
